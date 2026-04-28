008ba44bc352a6321fc68432df54d39014d32790
thejdmckinney <jeremy.mckinney@creativeconstructorsllc.com>
2026-04-27 21:27:30 -0500

    Add Tawk.to live chat widget to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,24 @@ export default function RootLayout({
         <main>{children}</main>
         <TestimonialsCarousel />
         <Footer />
+        
+        {/* Tawk.to Live Chat Widget */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69f01a81bdebb61c392b9bc0/1jn8uf2bq';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
